650272cf299ee9dc0b482c445c8489c42f1b3587
Hristo <ico.d.tonev@gmail.com>
2023-03-05 15:59:46 +0200

    refactor

--- tests/Unit/Policies/GamePolicyTest.php
+++ tests/Unit/Policies/GamePolicyTest.php
@@ -3,72 +3,78 @@
 namespace Tests\Unit\Policies;
 
 use App\Models\Game;
-use App\Models\Profile;
 use App\Models\User;
 use App\Policies\GamePolicy;
 use App\Services\Implementations\GameServiceImplementation;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Facades\Session;
 use Mockery;
 use PHPUnit\Framework\TestCase;
 
 class GamePolicyTest extends TestCase
 {
-    private $gamePolicy;
-
     protected function setUp(): void
     {
         parent::setUp();
-        $this->gamePolicy =
-            new GamePolicy(new GameServiceImplementation());
+        Session::shouldReceive('get')->andReturn(random_int(10000, 240000));
     }
 
     public function test_write_return_true_when_user_is_admin()
     {
-        $user = $this->mockUserHasRole(86, true);
+        $gamePolicy = $this
+            ->mockGameServiceGameBelongsToUser(false);
+        $user = $this->mockUserHasRole(true);
         $game = new Game();
 
-        $result = $this->gamePolicy->write($user, $game);
+        $result = $gamePolicy->write($user, $game);
 
         $this->assertTrue($result);
     }
 
     public function test_write_return_true_when_game_belong_to_user()
     {
-        $profileId = 123;
-        $user = $this->mockUserHasRole($profileId,  false);
-        $game = new Game(['profile_id' => $profileId]);
+        $gamePolicy = $this
+            ->mockGameServiceGameBelongsToUser(true);
+        $user = $this->mockUserHasRole(false);
+        $game = new Game();
 
-        $result = $this->gamePolicy->write($user, $game);
+        $result = $gamePolicy->write($user, $game);
 
         $this->assertTrue($result);
     }
 
     public function test_write_return_false_when_game_dont_belong_to_user()
     {
-        $user = $this->mockUserHasRole(343, false);
-        $game = new Game(['profile_id' => 123]);
+        $gamePolicy = $this
+            ->mockGameServiceGameBelongsToUser(false);
+        $user = $this->mockUserHasRole(false);
+        $game = new Game();
 
-        $result = $this->gamePolicy->write($user, $game);
+        $result = $gamePolicy->write($user, $game);
 
         $this->assertFalse($result);
     }
 
-    private function mockUserHasRole(int $profileId, bool $hasRoleReturn): User
+    private function mockUserHasRole(bool $hasRoleReturn): User
     {
         $user = new User();
-        Session::shouldReceive('get')->andReturn($profileId);
         $user = Mockery::mock($user);
         $user->shouldReceive('hasRole')->andReturn($hasRoleReturn);
 
         return $user;
     }
 
+    private function mockGameServiceGameBelongsToUser(bool $isGameBelongToUser): GamePolicy
+    {
+        $mockedGameService = Mockery::mock(GameServiceImplementation::class);
+        $mockedGameService->shouldReceive('isGameBelongsToUser')
+            ->andReturn($isGameBelongToUser);
+
+        return new GamePolicy($mockedGameService);
+    }
+
     protected function tearDown(): void
     {
         parent::tearDown();
-
-        Session::swap(null);
         Mockery::close();
     }
 }
